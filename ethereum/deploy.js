c6d0b42786f2409fec7b72bad19451305b93f078
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-09 10:58:03 +0700

    Ethereum Infra Done

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'finish initial buddy liberty blind opera popular jaguar switch ramp victory dentist',
+  'https://rinkeby.infura.io/v3/8b9216a5d2434898bfc43998c1e718bb'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
